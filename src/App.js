ba3a2f6868d3bfefe39701b6fe35e1dd610749e8
Liana <l.yukhymchak@gmail.com>
2023-04-20 22:07:51 +0200

    fixed git link

--- src/App.js
+++ src/App.js
@@ -7,13 +7,19 @@ function App() {
   return (
     <div className="App">
       <Weather />
-      <a
-        href="https://github.com/lyukhymchak/react-weather-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Git repo
-      </a>
+      <footer>
+        <div>© 2021 SheCodes All rights reserved.</div>
+        <small>
+          <a
+            href="https://github.com/lyukhymchak/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Open-source code
+          </a>
+          <span> by L. Y.</span>
+        </small>
+      </footer>
     </div>
   );
 }
